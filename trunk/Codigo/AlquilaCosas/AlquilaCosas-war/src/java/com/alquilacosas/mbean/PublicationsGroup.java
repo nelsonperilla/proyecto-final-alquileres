e414c20685f3084995eed0f4000cd6861aa8705a
alanAtala <alanAtala@2dcb284c-b0b4-48c6-0b81-bd193d5aceaa>
2011-08-05 06:28:41 +0000

    Se arreglo el bug de que si no hay imagenes para una publicacion saltaba una exception, ahora muestra una imagen especial para ese caso, se saco el stateful de mostrarPublicacionesBean y se implemento el uso de paso de parametros por url

--- trunk/Codigo/AlquilaCosas/AlquilaCosas-war/src/java/com/alquilacosas/mbean/PublicationsGroup.java
+++ trunk/Codigo/AlquilaCosas/AlquilaCosas-war/src/java/com/alquilacosas/mbean/PublicationsGroup.java
@@ -8,7 +8,14 @@ import com.alquilacosas.common.PublicacionFacade;
 import com.alquilacosas.ejb.entity.Publicacion;
 import com.alquilacosas.ejb.session.MostrarPublicacionesBeanLocal;
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -16,6 +23,7 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 /**
@@ -31,15 +39,30 @@ public class PublicationsGroup implements Serializable
     private List<PublicacionFacade> publicacionesRandom;
     private StreamedContent imagenPrincipal; 
     private String selectedPublicationId;
-
+    private byte[] imageNotfound;
     public PublicationsGroup() 
     {
 
     }
 
     @PostConstruct
-    public void init() 
+    public void init()
     {
+        try {
+            File fileImageNotFound=new File(((ServletContext)
+                 FacesContext.getCurrentInstance().getExternalContext().getContext()).getRealPath
+                 ("/resources/images/noImage.png"));
+            FileInputStream streamImageNotFound=new FileInputStream(fileImageNotFound);
+            imageNotfound=new byte[(int)fileImageNotFound.length()];
+            streamImageNotFound.read(imageNotfound);
+        } catch (FileNotFoundException ex) {
+            
+            Logger.getLogger(PublicationsGroup.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.print("El archivo sigue mal");
+        } catch (IOException ex1) {
+            Logger.getLogger(PublicationsGroup.class.getName()).log(Level.SEVERE, null, ex1);
+        }
+    
         publicacionesRandom= new ArrayList<PublicacionFacade>();
         List<Publicacion> listaPublicaciones = publicationBean.getPublicacionesRandom(0);
         for(Publicacion publicacion : listaPublicaciones)
@@ -48,8 +71,12 @@ public class PublicationsGroup implements Serializable
                     publicacion.getTitulo(),publicacion.getDescripcion(),
                     publicacion.getFechaDesde(),publicacion.getFechaHasta(),
                     publicacion.getDestacada(), publicacion.getCantidad());
-            String nombre=publicacion.getTitulo();
-            temp.setImagenes(publicationBean.getImage(publicacion.getPublicacionId()));
+            List<byte[]> imagenes=publicationBean.getImage(publicacion.getPublicacionId());
+            if(imagenes.isEmpty())
+            {
+                imagenes.add(imageNotfound);
+            }
+            temp.setImagenes(imagenes);
             publicacionesRandom.add(temp);
                     //publicacion.getCategoriaFk(),publicacion.getUsuarioFk()));
         }
@@ -69,31 +96,34 @@ public class PublicationsGroup implements Serializable
      * @return the imagenPrincipal
      */
     public StreamedContent getImagenPrincipal() {
-        
-        boolean h=FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().containsKey("h");
         String id=FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("photo_id");
         if(id!=null){
             int photo_id  = Integer.parseInt(id);
-            System.out.println(photo_id);
+            //System.out.println(photo_id);
             byte[] img=publicacionesRandom.get(photo_id-1).getImagenPrincipal();
             ByteArrayInputStream i = new ByteArrayInputStream(img);
             imagenPrincipal=new DefaultStreamedContent(i);        
         }
-        else 
-            System.out.println("noooooooooooo");
         return imagenPrincipal;
     }
+    
+    
 
     /**
      * @param imagenPrincipal the imagenPrincipal to set
      */
     public void setImagenPrincipal(StreamedContent imagenPrincipal) {
+        
         this.imagenPrincipal = imagenPrincipal;
     }
 
     /**
      * @return the selectedPublicationId
      */
+    public String seleccionarPublicacion() {
+        return "mostrarPublicacion?id=" +  selectedPublicationId;
+    }
+    
     public String getselectedPublicationId() {
         return selectedPublicationId;
     }
@@ -102,9 +132,10 @@ public class PublicationsGroup implements Serializable
      * @param selectedPublicationId the selectedPublicationId to set
      */
     public void setselectedPublicationId(String selectedPublicationId) {
-         
+         ///vistas/mostrarPublicacion.xhtm?id=#{publicacionMBean.id}
         this.selectedPublicationId = selectedPublicationId;
-        publicationBean.setSelectedPublication(Integer.parseInt(selectedPublicationId));
+        //publicationBean.setSelectedPublication(Integer.parseInt(selectedPublicationId));
+        //System.out.println(selectedPublicationId);
     }
    
 }
