dc90b8beb13590a4665a657dbfc1d21cff72becd
alanAtala <alanAtala@2dcb284c-b0b4-48c6-0b81-bd193d5aceaa>
2012-08-27 19:37:24 +0000

    Se corrigieron errores de ortografia

--- Codigo/AlquilaCosas/AlquilaCosas-ejb/src/java/com/alquilacosas/facade/AlquilerXEstadoFacade.java
+++ Codigo/AlquilaCosas/AlquilaCosas-ejb/src/java/com/alquilacosas/facade/AlquilerXEstadoFacade.java
@@ -4,12 +4,16 @@
  */
 package com.alquilacosas.facade;
 
-import com.alquilacosas.common.AlquilaCosasException;
 import com.alquilacosas.ejb.entity.Alquiler;
 import com.alquilacosas.ejb.entity.AlquilerXEstado;
 import com.alquilacosas.ejb.entity.EstadoAlquiler;
 import com.alquilacosas.ejb.entity.EstadoAlquiler.NombreEstadoAlquiler;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.Query;
 import javax.persistence.EntityManager;
@@ -56,4 +60,92 @@ public class AlquilerXEstadoFacade extends AbstractFacade<AlquilerXEstado> {
         alquiler.agregarAlquilerXEstado(estadoActual);
         em.persist(estadoActual);
     }
+
+    
+    
+
+        class RandomDateGenerator {
+
+        private Date sebelum;
+        private Date sesudah;
+        private int repetition;
+
+        public Date getSebelum() {
+            return sebelum;
+        }
+
+        public void setSebelum(Date sebelum) {
+            this.sebelum = sebelum;
+        }
+
+        public Date getSesudah() {
+            return sesudah;
+        }
+
+        public void setSesudah(Date sesudah) {
+            this.sesudah = sesudah;
+        }
+
+        public int getRepetition() {
+            return repetition;
+        }
+
+        public void setRepetition(int repetition) {
+            this.repetition = repetition;
+        }
+
+        public RandomDateGenerator() {
+        }
+
+        private Date getRandomDateBetween(Date from, Date to) {
+            Calendar cal = Calendar.getInstance();
+
+            cal.setTime(from);
+            BigDecimal decFrom = new BigDecimal(cal.getTimeInMillis());
+
+            cal.setTime(to);
+            BigDecimal decTo = new BigDecimal(cal.getTimeInMillis());
+
+            BigDecimal selisih = decTo.subtract(decFrom);
+            BigDecimal factor = selisih.multiply(new BigDecimal(Math.random()));
+
+            return new Date((factor.add(decFrom)).longValue());
+        }
+    }    
+    
+
+    
+    private List<Date> createDates() {
+        List<Date> dates = new ArrayList<Date>();
+        try {
+            RandomDateGenerator rdg = new RandomDateGenerator();
+            rdg.setSebelum(new SimpleDateFormat("dd MM yyyy").parse("01 06 2011"));
+            rdg.setSesudah(new SimpleDateFormat("dd MM yyyy").parse("01 05 2012"));
+            rdg.setRepetition(100);
+
+            for (int i = 0; i < 100; i++) {
+                dates.add(rdg.getRandomDateBetween(rdg.sebelum, rdg.sesudah));
+            }
+
+        } catch (Exception e) {
+        }
+        return dates;
+    }    
+    
+
+    
+    public void saveStateFurioso(Alquiler alquiler, NombreEstadoAlquiler nombreEstadoAlquiler) {
+        AlquilerXEstado estadoActual = new AlquilerXEstado();
+        estadoActual.setAlquilerFk(alquiler);
+
+        Query query = em.createNamedQuery("EstadoAlquiler.findByNombre");
+        query.setParameter("nombre", nombreEstadoAlquiler);
+        EstadoAlquiler estado = (EstadoAlquiler) query.getSingleResult();
+        estadoActual.setEstadoAlquilerFk(estado);
+        estadoActual.setFechaDesde(createDates().get(new Double(Math.random() * 100).intValue()));
+
+        alquiler.agregarAlquilerXEstado(estadoActual);
+        em.persist(estadoActual);
+    }    
+
 }
